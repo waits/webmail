fd390caae2d17d76156407815627ba6b557632d2
Dylan Waits <dylan@waits.io>
2019-01-22 23:52:40 -0800

    Parse base64 messages

--- maildir/parse.go
+++ maildir/parse.go
@@ -1,6 +1,8 @@
 package maildir
 
 import (
+	"encoding/base64"
+	"io"
 	"io/ioutil"
 	"log"
 	"mime"
@@ -9,6 +11,10 @@ import (
 	"net/mail"
 	"strings"
 )
+
+type header interface {
+	Get(string) string
+}
 
 // Parses multipart and quoted-printable mail bodies.
 func parseBody(msg *mail.Message) (body Body, err error) {
@@ -25,37 +31,35 @@ func parseBody(msg *mail.Message) (body Body, err error) {
 				break
 			}
 
-			raw, err := ioutil.ReadAll(p)
-			contentType, _, err := mime.ParseMediaType(p.Header.Get("Content-Type"))
-			if err != nil {
-				log.Println("[ERROR] maildir:", err)
-			}
-
-			if contentType == "text/html" {
-				body.HTML = string(raw)
-			} else {
-				body.Plain = string(raw)
-			}
+			decodePart(p.Header, p, &body)
 		}
 	} else {
-		var raw []byte
-		if strings.ToLower(msg.Header.Get("Content-Transfer-Encoding")) == "quoted-printable" {
-			raw, err = ioutil.ReadAll(quotedprintable.NewReader(msg.Body))
-		} else {
-			raw, err = ioutil.ReadAll(msg.Body)
-		}
-
-		contentType, _, err := mime.ParseMediaType(msg.Header.Get("Content-Type"))
-		if err != nil {
-			log.Println("[ERROR] maildir:", err)
-		}
-
-		if contentType == "text/html" {
-			body.HTML = string(raw)
-		} else {
-			body.Plain = string(raw)
-		}
+		decodePart(msg.Header, msg.Body, &body)
 	}
 
 	return
+}
+
+func decodePart(h header, body io.Reader, out *Body) {
+	var raw []byte
+	var err error
+
+	if strings.ToLower(h.Get("Content-Transfer-Encoding")) == "quoted-printable" {
+		raw, err = ioutil.ReadAll(quotedprintable.NewReader(body))
+	} else if strings.ToLower(h.Get("Content-Transfer-Encoding")) == "base64" {
+		raw, err = ioutil.ReadAll(base64.NewDecoder(base64.StdEncoding, body))
+	} else {
+		raw, err = ioutil.ReadAll(body)
+	}
+
+	contentType, _, err := mime.ParseMediaType(h.Get("Content-Type"))
+	if err != nil {
+		log.Println("[ERROR] maildir:", err)
+	}
+
+	if contentType == "text/html" {
+		out.HTML = string(raw)
+	} else {
+		out.Plain = string(raw)
+	}
 }
